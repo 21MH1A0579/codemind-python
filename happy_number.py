a730209feb0c08743f81595c95612fba6dd89c55
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-05-14 23:51:28 +0530

    def happy(n):     sm=0     while(n):         r=n%10         sm+=(r**2)         n=n//10     if(sm<9):         if(sm==1 or sm==7):             return True         else:             return False     else:         return happy(sm)
    n=int(input())
    print(happy(n))

--- happy_number.py
+++ happy_number.py
@@ -1,15 +1,19 @@
 def happy(n):
-    sm=0;
+    sm=0
     while(n):
         r=n%10
-        sm=sm+(r**2)
+        sm+=(r**2)
         n=n//10
-    if(sm>9):
-        return happy(sm)
-    else:
+    if(sm<9):
         if(sm==1 or sm==7):
             return True
         else:
             return False
+    else:
+        return happy(sm)
+        
 n=int(input())
 print(happy(n))
+            
+        
+        
