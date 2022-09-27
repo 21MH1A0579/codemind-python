7caf0fd4c1f3af94cff87cff738f6aa5cc6cfbd4
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-09-27 11:17:14 +0530

    def prime(n):     if(n==1):         return 0     for i in range(2,int(n**0.5)+1):         if (n%i==0):             return 0     else:         return 1 n=int(input()) while n:     a=int(input())     for i in range(a+1,99999999):         if prime(i):             x=i             break     for j in range(a,1,-1):         if prime(j):             y=j             break     if (x-a)<(a-y):         print(x)     else:         print(y)     n-=1

---
+++ Nearest_Prime.py
@@ -0,0 +1,27 @@
+def prime(n):
+    if(n==1):
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if (n%i==0):
+            return 0
+    else:
+        return 1
+n=int(input())
+while n:
+    a=int(input())
+    for i in range(a+1,99999999):
+        if prime(i):
+            x=i
+            break
+    for j in range(a,1,-1):
+        if prime(j):
+            y=j
+            break
+    if (x-a)<(a-y):
+        print(x)
+    else:
+        print(y)
+    n-=1
+            
+        
+        
