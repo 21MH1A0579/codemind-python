0d4bd70f34b3be3011b32b27b97ac1cc19352256
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-09-27 11:26:03 +0530

    def prime(n):     if(n==1):         return 0     for i in range(2,(n//2)+1):         if (n%i==0):             return 0     else:         return 1 a=int(input()) for i in range(a+1,99999999):     if prime(i):         x=i         break for j in range(a,2,-1):     if prime(j):         y=j         break if (x-a)<(a-y):     print(x-a) else:     print(a-y)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,23 @@
+def prime(n):
+    if(n==1):
+        return 0
+    for i in range(2,(n//2)+1):
+        if (n%i==0):
+            return 0
+    else:
+        return 1
+a=int(input())
+for i in range(a+1,99999999):
+    if prime(i):
+        x=i
+        break
+for j in range(a,2,-1):
+    if prime(j):
+        y=j
+        break
+if (x-a)<(a-y):
+    print(x-a)
+else:
+    print(a-y)
+        
+        
